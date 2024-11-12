fdd38a8978c31700741545bda58394a25cee05e0
Meazajao <153733325+Meazajao@users.noreply.github.com>
2024-11-12 20:38:15 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+
+
+const section = document.querySelector("section");
+const playerLivesCount = document.querySelector("span");
+const playerLives = 6;
+
+playerLivesCount.textContent = playerLives;
+
+// generate data
+
+const getData = () => [
+    { imgSrc: "./images"}
+]
